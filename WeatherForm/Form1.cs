c9df76ac83c83d4fee309fbf7cb5c62ea0ae4bd0
Evan <complicated465@hotmail.com>
2016-06-30 15:20:36 +0800

    First Commit

---
+++ WeatherForm/Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WeatherForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+            WeatherCrawler myWeatherCrawler = new WeatherCrawler(this);
+            myWeatherCrawler.craw();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        internal void update(object allData)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WeatherForm/WeatherCrawler.cs
@@ -0,0 +1,48 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net;
+using System.Text;
+
+namespace WeatherForm
+{
+    class WeatherCrawler
+    {
+        private readonly Form1 _form;
+
+        public WeatherCrawler(Form1 form)
+        {
+            this._form = form;
+        }
+
+        public void craw()
+        {
+            int count = 0;
+            int offset = 0;
+            int limit = 100;
+            int first = 0;
+            WebClient client = new WebClient();
+           
+            StreamWriter sw1 = new StreamWriter(System.Windows.Forms.Application.StartupPath + @"\WeatherData.txt", false, Encoding.UTF8);
+
+            MemoryStream ms = new MemoryStream(client.DownloadData("http://www.cwb.gov.tw/V7/forecast/town368/"));
+
+            HtmlDocument doc = new HtmlDocument();
+            doc.Load(ms, Encoding.UTF8);
+
+
+
+            
+            sw1.Close();
+            
+            
+
+
+            Console.WriteLine("sdfsadf");
+
+            // _form.update("ffff");
+            //_form.update(allData);
+        }
+
+    }
+}
